060c64c1085c8389176eb9329235a5270299d776
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2023-03-16 09:11:59 +0530

    adding both opencv version and tensorflow version to github

---
+++ anpr-opencv/number_plate.py
@@ -0,0 +1,34 @@
+import cv2
+haarcascade = "./model/haarcascade_russian_plate_number.xml"
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)  # width
+cap.set(4, 480)  # height
+min_area = 500
+count = 0
+while True:
+    success, img = cap.read()
+    plate_cascade = cv2.CascadeClassifier(haarcascade)
+    image_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    plate = plate_cascade.detectMultiScale(image_gray, 1.1, 4)
+    for (x, y, w, h) in plate:
+        area = w*h
+
+        if area > min_area:
+            cv2.rectangle(img, (x, y), (x+w, y+h), (255, 255, 0), 2)
+            cv2.putText(img, "Number Plate", (x, y-5),
+                        cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255, 0, 255), 2)
+            img_roi = img[y:y+h, x: x+w]
+            cv2.imshow("ROI", img_roi)
+    cv2.imshow("Result", img)
+    if cv2.waitKey(1) & 0xFF == ord('s'):
+        cv2.imwrite("./Resources/plate_img/scanned_img_" +
+                    str(count)+".jpg", img_roi)
+        cv2.rectangle(img, (0, 200), (640, 300), (255, 255, 0), cv2.FILLED)
+        cv2.putText(img, "Plate saved", (150, 265),
+                    cv2.FONT_HERSHEY_COMPLEX_SMALL, 2, (0, 0, 255), 2)
+        cv2.imshow("Results", img)
+        cv2.waitKey(500)
+        count += 1
+    # close camera
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
